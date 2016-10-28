3c314405aa541bafcfaa52859cfcbfc0a029183b
wangyinnian <wangyinnian@autorock.com.cn>
2016-10-28 14:22:45 +0800

    uboot-imx-imx_v2015.04_4.1.15_1.0.0_ga porting to autorock wisehmi boards

---
+++ build_uboot_clean.sh
@@ -0,0 +1,3 @@
+make ARCH=arm CROSS_COMPILE=arm-buildroot-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-buildroot-linux-gnueabihf- mx6dlwisehmi_ddr128x32_nand_defconfig
+make ARCH=arm CROSS_COMPILE=arm-buildroot-linux-gnueabihf- 
